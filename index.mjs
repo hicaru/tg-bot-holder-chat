d30ff1ba397f11f43ae7dd360737575a7fe13bbd
Hicaru <hicc0rus@gmail.com>
2022-10-12 12:23:44 +0300

    installed dependencies

--- index.mjs
+++ index.mjs
@@ -1,10 +1,52 @@
 import { config } from 'dotenv';
-import telegramAPI from 'node-telegram-bot-api';
+import TelegramBot from 'node-telegram-bot-api';
 
 
 config();
 
-const API_KEY = process.env.TOKEN;
+const token = process.env.TOKEN;
+const bot = new TelegramBot(token, {
+  polling: true
+});
 
+// bot.createChatInviteLink // create privete link.
+// bot.banChatMember
+// bot.kickChatMember
 
-console.log(API_KEY);
+bot.onText(/\/echo (.+)/, async (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+  const chatId = msg.chat.id;
+  const hours = 1;
+  const date1 = new Date();
+  const dateToMilliseconds = date1.getTime();
+  const addedHours = dateToMilliseconds + (3600000 * hours);
+  const newDate = new Date(addedHours);
+
+  const link = await bot.createChatInviteLink(chatId, undefined, newDate.getTime(), 1);
+
+  console.log(link);
+
+  bot.revokeChatInviteLink
+
+  bot.sendMessage(chatId, `link: ${link.invite_link}`);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+// bot.on('message', async (msg) => {
+//   const chatId = msg.chat.id;
+//   console.log(msg);
+//   const hours = 1;
+//   const date1 = new Date();
+//   const dateToMilliseconds = date1.getTime();
+//   const addedHours = dateToMilliseconds + (3600000 * hours);
+//   const newDate = new Date(addedHours);
+
+//   const link = await bot.createChatInviteLink(chatId, 'Test', newDate.getTime(), 1);
+
+//   console.log(link);
+
+//   bot.sendMessage(chatId, `link: ${link.invite_link}, name: ${link.name}`);
+// });
