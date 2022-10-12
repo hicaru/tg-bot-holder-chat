223655085c8e959ed95b7f168d0036df0c0769e5
Hicaru <hicc0rus@gmail.com>
2022-10-12 12:33:06 +0300

    added signature verify

---
+++ src/bot.mjs
@@ -0,0 +1,26 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+
+export class TelegramBot {
+  #token = '';
+
+  get token() {
+    return this.#token;
+  }
+
+  constructor(token) {
+    this.#token = token;
+  }
+
+  async generateLink(chatId) {
+    const chatId = msg.chat.id;
+    const hours = 1;
+    const date1 = new Date();
+    const dateToMilliseconds = date1.getTime();
+    const addedHours = dateToMilliseconds + (3600000 * hours);
+    const newDate = new Date(addedHours);
+    const link = await bot.createChatInviteLink(chatId, undefined, newDate.getTime(), 1);
+
+    return link;
+  }
+}
---
+++ src/verify-signature.mjs
@@ -0,0 +1,26 @@
+import { sha256 } from 'js-sha256';
+import { schnorr } from '@zilliqa-js/crypto';
+import { getAddressFromPublicKey } from '@zilliqa-js/crypto';
+
+
+export function verifySignature(
+  message,
+  publicKey,
+  signature,
+  address
+) {
+  const hashStr = sha256(message);
+  const hashBytes = Buffer.from(hashStr, 'hex');
+  const bytecSignature = schnorr.toSignature(signature);
+  const fromPubkey = getAddressFromPublicKey(publicKey);
+
+  if (String(fromPubkey).toLowerCase() !== String(address).toLowerCase()) {
+    throw new Error('incorrect address.');
+  }
+
+  return schnorr.verify(
+    hashBytes,
+    bytecSignature,
+    Buffer.from(publicKey, 'hex')
+  );
+}
